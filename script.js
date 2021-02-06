37f462580ddafee13bab86664c15655a6803bd7c
Urolovshohjahon <urolovshohjahon@gmail.com>
2021-02-06 14:18:37 +0500

    Updated

--- script.js
+++ script.js
@@ -5,23 +5,19 @@ let adding = (price) => {
     document.querySelector('.result').innerHTML = value;
 }
 
+var i = 0;
+let show = ()=>{
+    
+    if(i%2==0){
+        document.querySelector('.menu2').style.opacity = 1;
+        document.querySelector('.knopka').style.transform = "rotate(180deg)";
+        i++;
+    }
+    else{
+        document.querySelector('.menu2').style.opacity = 0;
+        document.querySelector('.knopka').style.transform = "rotate(0deg)";
+        i++;
+    }
 
-//let adding = () => {
-//    let tugma = document.querySelector('.tugma')
-//    let value = document.querySelector('.result').innerHTML;
-//    let calculate = (price)=>{
-//        for(let j=500;j<price;j+=500){
-//
-//        }
-//    }
-//    var i = 0;
-//    if (i > 0) return 0;
-//    setInterval(() => {
-//
-//        value = value + '+' + 500;
-//        value = eval(value);
-//        document.querySelector('.result').innerHTML = value;
-//
-//    }, 50);
-//    i++;
-//}
+}
+
