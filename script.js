8a6131fd1903ef88a9a57315bf31150feb098487
Urolovshohjahon <urolovshohjahon@gmail.com>
2021-02-10 11:59:13 +0500

    Updated

--- script.js
+++ script.js
@@ -9,12 +9,14 @@ var i = 0;
 let show = ()=>{
     
     if(i%2==0){
-        document.querySelector('.menu2').style.opacity = 1;
+        document.querySelector('.menu2').style.display = "flex";
+        document.querySelector('header').style.transform = "translateX(-150px)";
         document.querySelector('.knopka').style.transform = "rotate(180deg)";
         i++;
     }
     else{
-        document.querySelector('.menu2').style.opacity = 0;
+        document.querySelector('.menu2').style.display = "none";
+        document.querySelector('header').style.transform = "translateX(0px)";
         document.querySelector('.knopka').style.transform = "rotate(0deg)";
         i++;
     }
